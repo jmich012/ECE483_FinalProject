9885ddd5fc58fcedabf035cb34278dcb5d551cfd
Jacob M <jmich012@odu.edu>
2023-12-06 20:03:57 -0500

    updated code

--- battery.py
+++ battery.py
@@ -1,10 +1,15 @@
 class battery:
     def __init__(self):
         self.MAX_BATTERY = 100.0
-        self.batter_level = 0.0
+        self.batteryLevel = 0.0
         self.continueSending = False
+        self.factor = 1
 
-    def updateStorage(self, kiloWattsGenerated):
+    def updateStorage(self, kiloWattsGenerated, peak):
+        if peak:
+            self.factor = 2
+        else:
+            self.factor = 1
         self.batteryLevel += kiloWattsGenerated
         if(self.batteryLevel > self.MAX_BATTERY * 0.8):
             self.sendPower()
@@ -18,14 +23,14 @@ class battery:
     def sendPower(self):
         # if after sending power to the building and the remaing battery level is
         # above 80% then sell power
-        if(self.battery - 2.5 > self.MAX_BATTERY * 0.8):
-            self.battery -= 4.0
+        if(self.batteryLevel - 2.5 > self.MAX_BATTERY * 0.8):
+            self.batteryLevel -= 4.0 * self.factor
         # check if after sending power if above the minimum threshold then continue
         # to use battery power
-        elif(self.battery - 2.5 < self.MAX_BATTERY * 0.8 and self.battery - 2.5 > self.MAX_BATTERY * 0.25):
-            self.battery -= 2.5
+        elif(self.batteryLevel - 2.5 < self.MAX_BATTERY * 0.8 and self.batteryLevel - 2.5 > self.MAX_BATTERY * 0.25):
+            self.batteryLevel -= 2.5 * self.factor
             self.continueSending = True
-        elif(self.battery - 2.5 < self.MAX_BATTERY * 0.25):
+        elif(self.batteryLevel - 2.5 < self.MAX_BATTERY * 0.25):
             self.continueSending = False
 
 
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from ext_power_gen import ext_power_gen
 from battery import battery
 import LCD
@@ -8,11 +9,65 @@ bat = battery()
 
 useGeneratedPower = False
 
-led = LEDR.open_dev()
-lcd = LCD.open_dev()
+LEDR.open_dev()
+LCD.open_dev()
 
-def updateDisplay():
-    pass
+"""
+needed for the writing to the LCD screen
+"""
+screen_x, screen_y, char_x,char_y = LCD.read()
+LCD.erase()
+LCD.clear()
+
+
+
+def updateDisplay(solar, wind, batteryLevel, useGeneratedPower, peak):
+    # build the battery percentage msg
+    msg = "Battery: " + str(batteryLevel)
+    LCD.text(0, char_y -1, msg)
+
+    # msg for if grid power is being used
+    if(useGeneratedPower):
+        msg = "Grid Power: N"
+    else:
+        msg = "Grid Power: Y"
+
+    LCD.text(0,char_y - 2, msg)
+
+    # msg for if battery power is being used
+    if(useGeneratedPower):
+        msg = "Battery Power: Y"
+    else:
+        msg = "Battery Power: N"
+
+    LCD.text(0,char_y - 3, msg)
+
+
+    # msg for peak power usage
+    if(peak):
+        msg = "Peak Usage: Y"
+    else:
+        msg = "Peak Usage: N"
+
+    LCD.text(0,char_y - 4, msg)
+
+    # msg for high or low wind gen
+    msg = "Wind: MIN"
+    if(wind == 3.0):
+        msg = "Wind: MAX"
+
+    LCD.text(0,char_y - 5, msg)
+
+    # msg for high or low solar gen
+    msg = "Solar: MIN"
+    if(solar == 3.0):
+        msg = "Solar: MAX"
+
+    LCD.text(0,char_y - 6, msg)
+
+    LCD.show()
+
+
 
 while 1:
     # get the current values of the solar panels and wind turbine
@@ -20,5 +75,6 @@ while 1:
     totalGeneratedPower = solar + wind
 
     # update the battery storage
-    batteryLevel, useGeneratedPower = bat.updateStorage(totalGeneratedPower)
+    batteryLevel, useGeneratedPower = bat.updateStorage(totalGeneratedPower, False)
 
+    updateDisplay(solar,wind,batteryLevel, useGeneratedPower, False)
