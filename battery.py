2e3ba35028687a8f3e451465679e4d9bf2922e32
Duncan-Alex-Clark <88975140+Duncan-Alex-Clark@users.noreply.github.com>
2023-12-06 21:55:53 -0500

    Refactor

--- battery.py
+++ battery.py
@@ -2,43 +2,62 @@ class battery:
     def __init__(self):
         self.MAX_BATTERY = 100.0
         self.batteryLevel = 0.0
-        self.continueSending = False
+        self.sending = False
         self.factor = 1
+        self.upperThreshold = self.MAX_BATTERY * 0.8
+        self.lowerThreshold = self.MAX_BATTERY * 0.25
 
     def updateStorage(self, kiloWattsGenerated, peak):
-        if peak != 0:
+        
+        # Set the depletion factor based on power demand
+        self.setFactor(peak)
+
+        # Increase the battery level by the power generated, set in main.py
+        self.batteryLevel += kiloWattsGenerated
+
+        # Determine if there is a surplus of battery storage and correct for MAX
+        self.determineSurplus()
+            
+        # Determine if the battery should be sending power
+        self.evalThreshold()
+        
+        # Consume power from the battery
+        if(self.sending):
+            self.sendPower()
+
+        return self.batteryLevel, self.sending
+    
+    
+    
+    
+    
+    
+    
+    def setFactor(self, peak):
+        if peak:
             self.factor = 2
         else:
             self.factor = 1
-
-        self.batteryLevel += kiloWattsGenerated
-
+    
+    def determineSurplus(self):
         if self.batteryLevel > self.MAX_BATTERY:
             self.batteryLevel = self.MAX_BATTERY
-            return self.batteryLevel, self.continueSending 
-
-        if(self.batteryLevel > self.MAX_BATTERY * 0.8):
-            self.sendPower()
-        elif(self.continueSending):
-            self.sendPower()
-        elif(self.batteryLevel < self.MAX_BATTERY * 0.25):
-            self.continueSending = False
-
-        return self.batteryLevel, self.continueSending
-
-
+    
+    def evalThreshold(self):
+        if(self.batteryLevel > self.upperThreshold):
+            self.sending = True
+        
+        if(self.batteryLevel < self.lowerThreshold):
+            self.sending = False
+    
+    # Draw power from the battery
     def sendPower(self):
-        # if after sending power to the building and the remaing battery level is
-        # above 80% then sell power
-        if(self.batteryLevel - 2.5 > self.MAX_BATTERY * 0.8):
-            self.batteryLevel -= 4.0 * self.factor
-        # check if after sending power if above the minimum threshold then continue
-        # to use battery power
-        elif(self.batteryLevel - 2.5 < self.MAX_BATTERY * 0.8 and self.batteryLevel - 2.5 > self.MAX_BATTERY * 0.25):
-            self.batteryLevel -= 2.5 * self.factor
-            self.continueSending = True
-        elif(self.batteryLevel - 2.5 < self.MAX_BATTERY * 0.25):
-            self.continueSending = False
+        baseConsumption = 4
+        consumption = baseConsumption * self.factor
+        self.batteryLevel -= consumption
+
+
+    
 
 
 
