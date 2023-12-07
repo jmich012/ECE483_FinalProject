8be2ca43f343bc6c2fa8ba9741ce0e650139b6d3
Duncan-Alex-Clark <88975140+Duncan-Alex-Clark@users.noreply.github.com>
2023-12-06 22:40:36 -0500

    adjustments

--- ext_power_gen.py
+++ ext_power_gen.py
@@ -2,8 +2,7 @@ import SW
 
 class ext_power_gen:
     def __init__(self):
-        self.solar = 1
-        self.wind = 1
+        self.factor = 0.8
 
     def getSWValue(self):
         val = SW.read()
@@ -16,20 +15,20 @@ class ext_power_gen:
     def generatePower(self):
         swValue = self.getSWValue()
         if(swValue == 0):
-            self.solar = 1.0
-            self.wind = 1.0
+            self.solar = 1.0 * self.factor
+            self.wind = 1.0 * self.factor
         elif(swValue == 1):
-            self.solar = 1.0
-            self.wind = 3.0
+            self.solar = 1.0 * self.factor
+            self.wind = 3.0 * self.factor
         elif(swValue == 2):
-            self.solar = 3.0
-            self.wind = 1.0
+            self.solar = 3.0 * self.factor
+            self.wind = 1.0 * self.factor
         elif(swValue == 3):
-            self.solar = 3.0
-            self.wind = 3.0
+            self.solar = 3.0 * self.factor
+            self.wind = 3.0 * self.factor
         else:
-            self.solar = 1.0
-            self.wind = 1.0
+            self.solar = 1.0 * self.factor
+            self.wind = 1.0 * self.factor
 
         return self.solar, self.wind
 
